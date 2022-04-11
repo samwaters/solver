6c79e425d41c639304e9550d357ad08d040e5466
Sam Waters <sam@samwaters.com>
2022-04-11 19:47:29 +0100

    Add tests for validity selector

--- src/components/Letter/__tests__/lettervalidityselector.test.tsx
+++ src/components/Letter/__tests__/lettervalidityselector.test.tsx
@@ -2,10 +2,11 @@ import * as React from 'react'
 import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { LetterValiditySelector } from 'components/Letter/lettervalidityselector'
 import { theme } from 'theme/theme'
 import { getStore } from 'utils/test.utils'
+import { SET_KNOWN_LETTER_VALIDITY } from 'actions/letters.actions'
 
 describe('components/letter/lettervalidityselector', () => {
     const store = getStore()
@@ -34,5 +35,46 @@ describe('components/letter/lettervalidityselector', () => {
         )
         const container = screen.getByTestId('letter-validity-selector')
         expect(store.getActions()).toStrictEqual([])
+        // Unknown
+        fireEvent.click(container.children[0])
+        expect(store.getActions()).toStrictEqual([
+            {
+                payload: {
+                    index: 0,
+                    letter: 'A',
+                    row: 1,
+                    valid: null,
+                },
+                type: SET_KNOWN_LETTER_VALIDITY,
+            },
+        ])
+        store.clearActions()
+        // Invalid
+        fireEvent.click(container.children[1])
+        expect(store.getActions()).toStrictEqual([
+            {
+                payload: {
+                    index: 0,
+                    letter: 'A',
+                    row: 1,
+                    valid: false,
+                },
+                type: SET_KNOWN_LETTER_VALIDITY,
+            },
+        ])
+        store.clearActions()
+        // Valid
+        fireEvent.click(container.children[2])
+        expect(store.getActions()).toStrictEqual([
+            {
+                payload: {
+                    index: 0,
+                    letter: 'A',
+                    row: 1,
+                    valid: true,
+                },
+                type: SET_KNOWN_LETTER_VALIDITY,
+            },
+        ])
     })
 })
