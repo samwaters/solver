b30430bb63a71fef3fa0bee4606d6d9daee43829
Sam Waters <sam@samwaters.com>
2022-04-16 03:01:57 +0100

    Fix tests

--- src/components/Icons/__tests__/icon.test.tsx
+++ src/components/Icons/__tests__/icon.test.tsx
@@ -1,23 +1,25 @@
 import * as React from 'react'
 import { act, render, screen, waitFor } from '@testing-library/react'
-import { Icon, IconColors, Icons } from 'components/Icons/icon'
+import { Icon, Icons } from 'components/Icons/icon'
+import BackspaceIcon from 'components/Icons/svg/backspace'
 import TrashIcon from 'components/Icons/svg/trash'
 
 describe('components/Icons', () => {
     it('Should render an icon', async () => {
         act(() => {
-            render(
-                <Icon
-                    color={IconColors.BLACK}
-                    height={20}
-                    icon={Icons.TRASH}
-                    width={20}
-                />
-            )
+            render(<Icon height={20} icon={Icons.TRASH} width={20} />)
         })
         await waitFor(() =>
             expect(screen.queryByTestId('icon-svg')).toBeInTheDocument()
         )
+    })
+
+    it('Renders the backspace icon', () => {
+        let icon
+        act(() => {
+            icon = render(<BackspaceIcon />)
+        })
+        expect(icon).toMatchSnapshot()
     })
 
     it('Renders the trash icon', () => {
